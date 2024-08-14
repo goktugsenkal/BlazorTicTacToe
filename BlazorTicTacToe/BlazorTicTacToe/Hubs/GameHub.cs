00bd4d65802fb93ebd7139cfdbb04251f2216c6f
Göktuğ Şenkal <goktugsenkal1881@gmail.com>
2024-08-14 16:07:37 +0300

    Add GameHub

--- BlazorTicTacToe/BlazorTicTacToe/Hubs/GameHub.cs
+++ BlazorTicTacToe/BlazorTicTacToe/Hubs/GameHub.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BlazorTicTacToe.Shared;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorTicTacToe.Hubs
 {
     public class GameHub : Hub
     {
+        private static readonly List<GameRoom> _rooms = new();
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"'{Context.ConnectionId}' ID'li oyuncu bağlandı.");
+
+            await Clients.Caller.SendAsync("Rooms", _rooms.OrderBy(x=>x.RoomName));
+        }
+
+        public async Task<GameRoom> CreateRoom(string roomName, string playerName)
+        {
+            var roomId = Guid.NewGuid().ToString();
+            var gameRoom = new GameRoom(roomId, roomName);
+            _rooms.Add(gameRoom);
+
+            await Clients.All.SendAsync("Rooms", _rooms.OrderBy(x => x.RoomName));
+
+            return gameRoom;
+        }
     }
 }
