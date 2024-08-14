34f6cd6a007eb7deb0e5592742dd7d18f836aba4
Göktuğ Şenkal <goktugsenkal1881@gmail.com>
2024-08-14 16:06:39 +0300

    Add shared project & entities

---
+++ BlazorTicTacToe.Shared/GameRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTicTacToe.Shared
+{
+    public class GameRoom
+    {
+        public string RoomId { get; set; }
+        public string RoomName { get; set; }
+
+        public GameRoom(string roomId, string roomName)
+        {
+            RoomId = roomId;
+            RoomName = roomName;
+        }
+    }
+}
